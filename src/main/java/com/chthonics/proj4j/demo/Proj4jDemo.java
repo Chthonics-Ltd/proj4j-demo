9c53e008d8cd743cb0778f3150af51bfcc656bab
Warwick Dufour <warwick.dufour@chthonics.com>
2025-03-13 11:35:26 +0000

    Initial - doesn't build yet

---
+++ src/main/java/com/chthonics/proj4j/demo/Proj4jDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.chthonics.proj4j.demo;
+
+/**
+ *
+ * @author warwick
+ */
+public class Proj4jDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
